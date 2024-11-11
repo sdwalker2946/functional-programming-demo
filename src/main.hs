c8cdf62603751a7f807040f030596ced03de076c
sdwalker <sdwalker@wm.edu>
2024-11-10 21:45:22 -0500

    Implemented all Haskell functions

--- src/main.hs
+++ src/main.hs
@@ -1,11 +1,29 @@
 -- Write printAMessage here
+printAMessage :: Show a => a -> IO ()
+printAMessage a = do print a
 
 -- Write division here
+division :: Double -> Double -> Maybe Double
+division x y
+    | x == y = Nothing
+    | otherwise = Just (x / y)
 
 -- Write factorial here
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial (n-1)
 
 -- Write factList here
+factList :: Int -> [Int]
+factList n = map factorial [1..n]
 
 -- Write merge here
+merge :: [Int] -> [Int] -> [Int]
+merge xs [] = xs
+merge [] ys = ys
+merge (x : xs) (y : ys)
+    | x <= y = x : merge xs (y : ys)
+    | otherwise = y : merge (x : xs) ys
 
-main = print "Test" -- Replace this with your testing code
+main = print (merge [1,3,5,7] [0,2,4,6,8])
+--print "Test" -- Replace this with your testing code
--- src/test.hs
+++ src/test.hs
@@ -1,2 +1,11 @@
 -- Feel free to experiment with Haskell in this file!
 -- This will not affect your assignment's grade
+addNums :: Int -> Int
+addNums 0 = 0
+addNums n = n + addNums (n-1)
+
+sumPowers :: Int -> [Int] -> Int
+sumPowers _ [] = 0
+sumPowers power (x : xs) = (x ^ power) + sumPowers power xs
+
+main = print (addNums 10)
